ddcb2706f1bbf83487437725be476ba28789eccc
Arif <arifcodes11@gmail.com>
2025-03-15 11:21:17 +0530

    Add Hello World examples in C++, Java, and Python with corresponding executables

---
+++ day1/first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+//javac first.java -- compilation
+//java first -- execution
