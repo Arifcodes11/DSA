0f710d3e3b058a354aaf686a54c29919e1c626bf
Arif <arifcodes11@gmail.com>
2025-03-15 11:50:20 +0530

    Remove Hello World examples in C++, Java, and Python

--- first.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout<<"Hello World";
-}
-//compile: g++ first.cpp -o first
-//run: ./first   .\first.exe
--- first.py
+++
@@ -1,4 +0,0 @@
-print("Hello, World!")
-
-
-#run the code
