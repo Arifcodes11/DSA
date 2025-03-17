08bb9c354ed546eb46031d7fb108e94795d7c8fa
Arif <arifcodes11@gmail.com>
2025-03-17 13:35:14 +0530

    Refactor pattern6.cpp for improved formatting and consistency

--- day3/pattern6.cpp
+++ day3/pattern6.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-
+int main()
+{
     int n;
     cin>>n;
 
-    int row = 1;
-    while(row <= n) {
-
-        int col = 1;
-
-        while(col <= n) {
-            char ch = 'A' + row - 1;
-            cout<< ch;
-            col = col + 1;
+    int row=1;
+    while(row<=n)
+    {
+        int col=1;
+        while(col<=n)
+        {
+            char ch='A'+row-1;
+            cout<<ch;
+            col++;
         }
         cout<<endl;
-        row = row + 1;
+        row++;
     }
     return 0;
 }
